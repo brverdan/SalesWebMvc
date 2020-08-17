905edb484c98f9dde9c971fc5a8305d019f1d1dc
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2020-08-16 22:18:49 -0300

    Revert "Foreign key not null (referential integrity)"
    This reverts commit c9a2e68649f9c03ed53f0d64be32877a0629e20e.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
-using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -12,12 +11,10 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
-            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -28,9 +25,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments };
-            return View(viewModel);
+            return View();
         }
 
         [HttpPost]
